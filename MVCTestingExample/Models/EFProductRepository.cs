ab45bcf8e62cb432fe2ce17a4aceb8a6109efbcc
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-11-18 17:53:53 -0800

    Implemented Product repository

--- MVCTestingExample/Models/EFProductRepository.cs
+++ MVCTestingExample/Models/EFProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using MVCTestingExample.Models.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using MVCTestingExample.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,30 +9,60 @@ namespace MVCTestingExample.Models
 {
     public class EFProductRepository : IProductRepository
     {
+        private readonly ProductDbContext _context;
 
+        public EFProductRepository(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Adds a product to the data store
+        /// </summary>        
         public Task AddProductAsync(Product p)
         {
-            throw new NotImplementedException();
+            _context.Add(p);
+            return _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Removes a product from the database
+        /// </summary>
         public Task DeleteProductAsync(Product p)
         {
-            throw new NotImplementedException();
+            _context.Remove(p);
+            return _context.SaveChangesAsync();
         }
 
-        public Task<List<Product>> GetAllProductsAsync()
+        /// <summary>
+        /// Returns a list of all products in the database
+        /// </summary>
+        public async Task<List<Product>> GetAllProductsAsync()
         {
-            throw new NotImplementedException();
+            return await _context.Products
+                           .OrderBy(p => p.Name)
+                           .ToListAsync();
         }
-
-        public Task<Product> GetProductByIdAsync(int id)
+        
+        /// <summary>
+        /// Returns a single product by id
+        /// or NULL if no product exists
+        /// </summary>
+        public async Task<Product> GetProductByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Products
+                           .Where(p => p.ProductId == id)
+                           .SingleOrDefaultAsync();
         }
 
+        /// <summary>
+        /// Used to updayte an existing product
+        /// in the database
+        /// </summary>
         public Task UpdateProductAsync(Product p)
         {
-            throw new NotImplementedException();
+            _context.Entry(p).State = EntityState.Modified;
+            return _context.SaveChangesAsync();
         }
     }
 }
